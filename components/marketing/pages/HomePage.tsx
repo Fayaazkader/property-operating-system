ed0596f7cd6f28dc774c3c3ef7afb6ab41f0b4d7
Fayaaz Kader <YOUR_GITHUB_EMAILfayaaz318@gmail.com>
2026-07-30 13:54:44 +0200

    Fix: remove EverythingFlows (old solution), keep new Solution section with animated cards and hub

--- components/marketing/pages/HomePage.tsx
+++ components/marketing/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import { Hero } from '../sections/Hero';
 import { Problem } from '../sections/Problem';
 import { Solution } from '../sections/Solution';
-import { EverythingFlows } from '../sections/EverythingFlows';
 import { Journey } from '../sections/Journey';
 import { Trust } from '../sections/Trust';
 import { CTA } from '../sections/CTA';
@@ -12,7 +11,6 @@ export function MarketingHomePage() {
       <Hero />
       <Problem />
       <Solution />
-      <EverythingFlows />
       <Journey />
       <Trust />
       <CTA />
